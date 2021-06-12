9f55ae93a545ea2bef59b682617b19815e56b5b0
andaraGui <guiandaradev@gmail.com>
2021-06-12 11:56:16 -0300

    filter cards/area and assets import

--- web/web/src/views/home/index.js
+++ web/web/src/views/home/index.js
@@ -1,8 +1,43 @@
+import React, {useState} from 'react';
+import * as S from './styles';
+
+//COMPONENTES
+import Header from '../../components/Header';
+import Footer from '../../components/Footer';
+import FilterCard from '../../components/FilterCard';
 
 function Home() {
-  return (
-    <h1>Você esta na home!</h1>
-  );
+    const [filterActived, setFilterActived] = useState('today');
+    return (
+        <S.Container>
+            <Header/>     
+            
+            <S.FilterArea>
+                <button type="button" onClick={ () => setFilterActived("all")} >
+                <   FilterCard title="Todos"   actived={filterActived === "all"} />
+                </button>
+                <button type="button" onClick={ () => setFilterActived("today")}>
+                    <FilterCard title="Hoje"   actived={filterActived === "today"} />
+                </button>
+                <button type="button" onClick={ () => setFilterActived("week")}>
+                    <FilterCard title="Semana" actived={filterActived === "week"} />
+                </button>
+                <button type="button" onClick={ () => setFilterActived("month")}>
+                    <FilterCard title="Mês"    actived={filterActived === "month"} />
+                </button>
+                <button type="button"  onClick={ () => setFilterActived("year")}>
+                    <FilterCard title="Ano"    actived={filterActived === "year"}/>
+                </button>
+                
+                
+                
+                
+                
+            </S.FilterArea>
+
+            <Footer/>       
+        </S.Container> 
+    )
 }
 
 export default Home;
---
+++ web/web/src/views/home/styles.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    width: 100%;
+`
+
+export const FilterArea = styled.div`
+
+    width: 94%;
+    display: flex;
+    justify-content: space-around;
+    margin-top: 30px;
+    margin-left: 3%;
+    
+    
+    button{
+        width: 30%;
+        background: none;
+        border: none;
+    }
+`
